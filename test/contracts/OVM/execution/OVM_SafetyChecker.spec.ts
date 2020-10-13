9c23aac8fe0416d5c849025df653066318530c11
Kevin Ho <kevinjho1996@gmail.com>
2020-10-12 22:09:03 -0400

    update safety checker: blacklist revert, KALL + KOPY checks

--- test/contracts/OVM/execution/OVM_SafetyChecker.spec.ts
+++ test/contracts/OVM/execution/OVM_SafetyChecker.spec.ts
@@ -7,7 +7,7 @@ import { Contract } from 'ethers'
 /* Internal Imports */
 import { SAFETY_CHECKER_TEST_JSON } from '../../../data'
 
-describe('OVM_SafetyChecker', () => {
+describe.only('OVM_SafetyChecker', () => {
   let OVM_SafetyChecker: Contract
   before(async () => {
     const Factory__OVM_SafetyChecker = await ethers.getContractFactory(
@@ -19,12 +19,14 @@ describe('OVM_SafetyChecker', () => {
 
   describe('isBytecodeSafe()', () => {
     for (const testName of Object.keys(SAFETY_CHECKER_TEST_JSON)) {
-      const test = SAFETY_CHECKER_TEST_JSON[testName]
-      it(`should correctly classify: ${testName}`, async () => {
-        expect(await OVM_SafetyChecker.isBytecodeSafe(test.in)).to.equal(
-          test.out
-        )
-      })
+      // if (testName.indexOf('call') >= 0) {
+        const test = SAFETY_CHECKER_TEST_JSON[testName]
+        it(`should correctly classify: ${testName}`, async () => {
+          expect(await OVM_SafetyChecker.isBytecodeSafe(test.in)).to.equal(
+            test.out
+          )
+        })
+      // }
     }
   })
 })
