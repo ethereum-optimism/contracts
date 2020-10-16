31fd1b87ecbc8647af49a54bd72939f6689a0631
B T <oliphant@Bs-MacBook-Pro.local>
2020-10-15 21:16:55 -0400

    workinggit status

---
+++ test/contracts/OVM/execution/OVM_ExecutionManager/print-methods.spec.ts
@@ -0,0 +1,36 @@
+import { expect } from '../../../../setup'
+
+/* External Imports */
+import { ethers } from '@nomiclabs/buidler'
+import { Contract, ContractFactory } from 'ethers'
+
+/* Internal Imports */
+import { SAFETY_CHECKER_TEST_JSON } from '../../../../data'
+
+describe('EM MethodIds', () => {
+  let Factory__OVM_ExecutionManager: ContractFactory
+  before(async () => {
+    Factory__OVM_ExecutionManager = await ethers.getContractFactory(
+      'OVM_ExecutionManager'
+    )
+
+  })
+    it.only('print ovmREVERT', async () => {
+      console.log(
+        Factory__OVM_ExecutionManager.interface.encodeFunctionData(
+          'ovmREVERT',
+          ['0x4204206969deadbeef']
+        )
+      )
+    })
+
+    it('print all', async () => {
+        for (let frag of Factory__OVM_ExecutionManager.interface.fragments) {
+            try {
+              console.log(`func ${frag.name}: ${Factory__OVM_ExecutionManager.interface.getSighash(frag as any)}`)
+            } catch {
+              console.log(`failed to print methodId for fragment ${frag.name}`)
+            }
+        }
+    })
+})
