ebd7a9152412a01b6ad8f29ff514713938d78c8e
Kevin Ho <kevinjho1996@gmail.com>
2020-11-10 19:07:53 -0500

    update tests

--- test/contracts/OVM/accounts/OVM_ECDSAContractAccount.spec.ts
+++ test/contracts/OVM/accounts/OVM_ECDSAContractAccount.spec.ts
@@ -27,6 +27,8 @@ const callPrecompile = async (
   )
 }
 
+const LEFTOVER_GAS = 2000
+
 describe('OVM_ECDSAContractAccount', () => {
   let wallet: Wallet
   let badWallet: Wallet
@@ -89,7 +91,9 @@ describe('OVM_ECDSAContractAccount', () => {
       )
 
       const ovmCALL: any = Mock__OVM_ExecutionManager.smocked.ovmCALL.calls[0]
-      expect(ovmCALL._gasLimit).to.equal(DEFAULT_EIP155_TX.gasLimit)
+      expect(ovmCALL._gasLimit).to.equal(
+        DEFAULT_EIP155_TX.gasLimit - LEFTOVER_GAS
+      )
       expect(ovmCALL._address).to.equal(DEFAULT_EIP155_TX.to)
       expect(ovmCALL._calldata).to.equal(DEFAULT_EIP155_TX.data)
 
@@ -116,7 +120,9 @@ describe('OVM_ECDSAContractAccount', () => {
       )
 
       const ovmCALL: any = Mock__OVM_ExecutionManager.smocked.ovmCALL.calls[0]
-      expect(ovmCALL._gasLimit).to.equal(DEFAULT_EIP155_TX.gasLimit)
+      expect(ovmCALL._gasLimit).to.equal(
+        DEFAULT_EIP155_TX.gasLimit - LEFTOVER_GAS
+      )
       expect(ovmCALL._address).to.equal(DEFAULT_EIP155_TX.to)
       expect(ovmCALL._calldata).to.equal(DEFAULT_EIP155_TX.data)
 
