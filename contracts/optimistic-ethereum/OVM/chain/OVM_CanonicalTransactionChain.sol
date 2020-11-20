57a0fd762e6891880fcf84951a193d2b6b8b7c80
Mark Tyneway <mark.tyneway@gmail.com>
2020-11-20 09:06:01 -0800

    ctc: forceInclusionPeriodSecods + lastOVMTimestamp public

--- contracts/optimistic-ethereum/OVM/chain/OVM_CanonicalTransactionChain.sol
+++ contracts/optimistic-ethereum/OVM/chain/OVM_CanonicalTransactionChain.sol
@@ -52,8 +52,8 @@ contract OVM_CanonicalTransactionChain is iOVM_CanonicalTransactionChain, Lib_Ad
      * Variables *
      *************/
 
-    uint256 internal forceInclusionPeriodSeconds;
-    uint256 internal lastOVMTimestamp;
+    uint256 public forceInclusionPeriodSeconds;
+    uint256 public lastOVMTimestamp;
     Lib_RingBuffer.RingBuffer internal batches;
     Lib_RingBuffer.RingBuffer internal queue;
 
