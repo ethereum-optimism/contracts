403b1aeaf63fd41c11ac386f74504afc37111bad
axchu <alina@optimism.io>
2020-12-01 14:37:10 -0600

    custom safemath

--- contracts/optimistic-ethereum/OVM/accounts/OVM_ECDSAContractAccount.sol
+++ contracts/optimistic-ethereum/OVM/accounts/OVM_ECDSAContractAccount.sol
@@ -9,14 +9,13 @@ import { iOVM_ECDSAContractAccount } from "../../iOVM/accounts/iOVM_ECDSAContrac
 import { Lib_OVMCodec } from "../../libraries/codec/Lib_OVMCodec.sol";
 import { Lib_ECDSAUtils } from "../../libraries/utils/Lib_ECDSAUtils.sol";
 import { Lib_SafeExecutionManagerWrapper } from "../../libraries/wrappers/Lib_SafeExecutionManagerWrapper.sol";
-import "@openzeppelin/contracts/math/SafeMath.sol";
+import { Lib_SafeMathWrapper } from "../../libraries/wrappers/Lib_SafeMathWrapper.sol";
 
 /**
  * @title OVM_ECDSAContractAccount
  */
 contract OVM_ECDSAContractAccount is iOVM_ECDSAContractAccount {
 
-    using SafeMath for uint256;
     address constant ETH_ERC20_ADDRESS = 0x4200000000000000000000000000000000000006;
     uint256 constant EXECUTION_VALIDATION_GAS_OVERHEAD = 25000; // TODO: should be the amount sufficient to cover the gas costs of all of the transactions up to and including the CALL/CREATE which forms the entrypoint of the transaction.
 
@@ -81,7 +80,7 @@ contract OVM_ECDSAContractAccount is iOVM_ECDSAContractAccount {
 
         // Need to make sure that the gas is sufficient to execute the transaction.
         Lib_SafeExecutionManagerWrapper.safeREQUIRE(
-           gasleft() >= SafeMath.add(decodedTx.gasLimit, EXECUTION_VALIDATION_GAS_OVERHEAD),
+           gasleft() >= Lib_SafeMathWrapper.add(decodedTx.gasLimit, EXECUTION_VALIDATION_GAS_OVERHEAD),
            "Gas is not sufficient to execute the transaction."
         );
 
@@ -97,7 +96,7 @@ contract OVM_ECDSAContractAccount is iOVM_ECDSAContractAccount {
         // Contract creations are signalled by sending a transaction to the zero address.
         if (decodedTx.to == address(0)) {
             address created = Lib_SafeExecutionManagerWrapper.safeCREATE(
-                decodedTx.gasLimit - 2000,
+                decodedTx.gasLimit,
                 decodedTx.data
             );
 
