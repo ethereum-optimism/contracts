2c35668b77a4e9e4d05fce305a6fe5bce48aaa39
Kelvin Fichter <kelvinfichter@gmail.com>
2020-12-08 15:56:21 -0500

    Have init set storage root

--- contracts/optimistic-ethereum/OVM/execution/OVM_StateManager.sol
+++ contracts/optimistic-ethereum/OVM/execution/OVM_StateManager.sol
@@ -275,7 +275,8 @@ contract OVM_StateManager is iOVM_StateManager {
     {
         Lib_OVMCodec.Account storage account = accounts[_address];
         account.nonce = 1;
-        account.codeHash = keccak256(hex'');
+        account.storageRoot = 0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421;
+        account.codeHash = 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470;
         account.isFresh = true;
     }
 
