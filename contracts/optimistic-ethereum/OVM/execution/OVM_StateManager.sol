c7a0d5d03b03612187846896de1e2cb44ded0d2c
Kelvin Fichter <kelvinfichter@gmail.com>
2020-12-08 21:31:35 -0500

    Another sm fix

--- contracts/optimistic-ethereum/OVM/execution/OVM_StateManager.sol
+++ contracts/optimistic-ethereum/OVM/execution/OVM_StateManager.sol
@@ -137,6 +137,7 @@ contract OVM_StateManager is iOVM_StateManager {
         public
         authenticated
     {
+        accounts[_address].storageRoot = 0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421;
         accounts[_address].codeHash = EMPTY_ACCOUNT_CODE_HASH;
     }
 
