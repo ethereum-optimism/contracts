16c2a3e64b7df6eef4bc8a5f3b4e1053e033ab92
Ben Jones <ben@pseudonym.party>
2020-12-14 17:57:10 -0800

    linting

--- test/contracts/mockOVM/verification/mockOVM_BondManager.spec.ts
+++ test/contracts/mockOVM/verification/mockOVM_BondManager.spec.ts
@@ -2,26 +2,12 @@ import { expect } from '../../../setup'
 
 /* External Imports */
 import { ethers } from '@nomiclabs/buidler'
-import { Signer, ContractFactory, Contract, BigNumber } from 'ethers'
-import { smockit, MockContract } from '@eth-optimism/smock'
+import { Signer, Contract } from 'ethers'
 
 /* Internal Imports */
 import {
   makeAddressManager,
-  setProxyTarget,
-  NON_NULL_BYTES32,
-  ZERO_ADDRESS,
-  NON_ZERO_ADDRESS,
-  NULL_BYTES32,
-  DUMMY_BATCH_HEADERS,
-  DUMMY_BATCH_PROOFS,
-  TrieTestGenerator,
-  toHexString,
-  getNextBlockNumber,
-  remove0x,
 } from '../../../helpers'
-import { getContractInterface } from '../../../../src'
-import { keccak256 } from 'ethers/lib/utils'
 
 describe('mockOVM_BondManager', () => {
     let sequencer: Signer
