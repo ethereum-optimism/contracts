02c178afbbdfdffda47b880fc4006892b033f9c9
axchu <alina@optimism.io>
2020-12-16 13:23:40 -0600

    add param

--- test/contracts/OVM/verification/OVM_StateTransitionerFactory.spec.ts
+++ test/contracts/OVM/verification/OVM_StateTransitionerFactory.spec.ts
@@ -43,7 +43,7 @@ describe('OVM_StateTransitionerFactory', () => {
 
       it('should revert', async () => {
         await expect(
-          OVM_StateTransitionerFactory.create(AddressManager.address, NULL_BYTES32, DUMMY_HASH)
+          OVM_StateTransitionerFactory.create(AddressManager.address, NULL_BYTES32, NULL_BYTES32, DUMMY_HASH)
         ).to.be.revertedWith(
           'Create can only be done by the OVM_FraudVerifier.'
         )
