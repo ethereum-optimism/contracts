9b997adda8ac11e461c99f289a16ca118d002969
Ben Jones <ben@pseudonym.party>
2021-01-11 19:06:37 -0800

    update MIN_ROLLUP_TX_GAS

--- contracts/optimistic-ethereum/OVM/chain/OVM_CanonicalTransactionChain.sol
+++ contracts/optimistic-ethereum/OVM/chain/OVM_CanonicalTransactionChain.sol
@@ -26,7 +26,7 @@ contract OVM_CanonicalTransactionChain is iOVM_CanonicalTransactionChain, Lib_Ad
      *************/
 
     // L2 tx gas-related
-    uint256 constant public MIN_ROLLUP_TX_GAS = 20000;
+    uint256 constant public MIN_ROLLUP_TX_GAS = 100000;
     uint256 constant public MAX_ROLLUP_TX_SIZE = 10000;
     uint256 constant public L2_GAS_DISCOUNT_DIVISOR = 32;
 
