70ab2a09ae4dd2d490084a8374d7e5300a721adb
Maurelian <maurelian@protonmail.ch>
2021-01-25 10:26:24 -0500

    Update contracts/optimistic-ethereum/OVM/execution/OVM_SafetyChecker.sol
    Co-authored-by: ben-chain <ben@pseudonym.party>

--- contracts/optimistic-ethereum/OVM/execution/OVM_SafetyChecker.sol
+++ contracts/optimistic-ethereum/OVM/execution/OVM_SafetyChecker.sol
@@ -10,7 +10,8 @@ import { iOVM_SafetyChecker } from "../../iOVM/execution/iOVM_SafetyChecker.sol"
  * "unsafe" opcodes. An opcode is considered unsafe if it would access state variables which
  * are specific to the environment (ie. L1 or L2) in which it is executed, as this could be used
  * to "escape the sandbox" of the OVM, resulting in non-deterministic fraud proofs. 
- * That is, an attacker wouuld be able to "prove fraud" on an honestly applied transaction.
+ * That is, an attacker would be able to "prove fraud" on an honestly applied transaction.
+ * Note that a "safe" contract requires opcodes to appear in a particular pattern; omission of "unsafe" opcodes is necessary, but not sufficient.
  *
  * This contract compiles to EVM bytecode.
  * It is only deployed on both Layer 1 and Layer 2. 
