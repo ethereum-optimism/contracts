68aa6865fb0fedf3324c45af28d2ef565d1010cf
Maurelian <maurelian@protonmail.ch>
2021-02-09 16:16:04 -0500

    quick save

--- test/contracts/OVM/bridge/ERC20/OVM_L1ERC20Gateway.spec.ts
+++ test/contracts/OVM/bridge/ERC20/OVM_L1ERC20Gateway.spec.ts
@@ -23,3 +23,46 @@ import {
 import { getContractInterface } from '../../../../../src'
 import { keccak256 } from 'ethers/lib/utils'
 
+
+const HARDCODED_GASLIMIT = 420069
+const decimals = 1 // what to set this to?
+
+describe.only('OVM_L1ERC20Gateway', () => {
+  // init signers
+  let alice: Signer
+  let bob: Signer
+
+  // we can just 
+  const mockL2ERC20Gateway: string = '0x1234123412341234123412341234123412341234'
+
+  let mockL1CrossDomainMessengerAddress: string
+  let Mock__OVM_L1CrossDomainMessenger: MockContract
+  let OVM_L2ERC20Gateway: Contract
+
+  let Factory__OVM_L1ERC20Gateway: ContractFactory
+  before(async () => {
+    Factory__OVM_L1ERC20Gateway = await ethers.getContractFactory('OVM_L1ERC20Gateway')
+  })
+
+
+
+  beforeEach(async () => {
+    let signer
+    ;[signer, alice, bob] = await ethers.getSigners()
+    mockL2CrossDomainMessengerAddress = await signer.getAddress() // though we don't keep track of the signer here, it needs to have come from an ethers signer so that {from: mockL2...} will work.
+
+    Mock__OVM_L2CrossDomainMessenger = await smockit(
+      await ethers.getContractFactory('OVM_L2CrossDomainMessenger'),
+      { address: mockL2CrossDomainMessengerAddress }
+    )
+    OVM_L2ERC20Gateway = await(
+      await ethers.getContractFactory('OVM_L2ERC20Gateway')
+    ).deploy(
+      Mock__OVM_L2CrossDomainMessenger.address,
+      'ovmWETH',
+      decimals
+    )
+
+    await OVM_L2ERC20Gateway.init(mockL1ERC20Gateway)
+  })
+
