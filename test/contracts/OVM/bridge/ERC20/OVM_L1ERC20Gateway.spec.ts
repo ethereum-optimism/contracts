f408a37ab64c74fae8e91f85f9ed9cd50ec3e9fe
Maurelian <maurelian@protonmail.ch>
2021-02-10 14:56:33 -0500

    l1 gateway testing WIP

--- test/contracts/OVM/bridge/ERC20/OVM_L1ERC20Gateway.spec.ts
+++ test/contracts/OVM/bridge/ERC20/OVM_L1ERC20Gateway.spec.ts
@@ -3,7 +3,7 @@ import { expect } from '../../../../setup'
 /* External Imports */
 import { ethers } from 'hardhat'
 import { Signer, ContractFactory, Contract, BigNumber } from 'ethers'
-import { smockit, MockContract } from '@eth-optimism/smock'
+import { smockit, MockContract, smoddit, ModifiableContract } from '@eth-optimism/smock'
 
 /* Internal Imports */
 import {
@@ -17,9 +17,6 @@ const ERR_INVALID_MESSENGER = 'OVM_XCHAIN: messenger contract unauthenticated'
 const ERR_INVALID_X_DOMAIN_MSG_SENDER = 'OVM_XCHAIN: wrong sender of cross-domain message'
 const MOCK_L1GATEWAY_ADDRESS: string = '0x1234123412341234123412341234123412341234'
 
-const HARDCODED_GASLIMIT = 420069
-const decimals = 1 // what to set this to?
-
 describe.only('OVM_L1ERC20Gateway', () => {
   // init signers
   let alice: Signer
@@ -30,13 +27,24 @@ describe.only('OVM_L1ERC20Gateway', () => {
 
 
   let Mock__OVM_L2ERC20Gateway: MockContract
-  let Mock__WETH: MockContract // or Mock contract? 
+  let Factory__L1ERC20: ContractFactory
+  let L1ERC20: Contract
+  const initialSupply = 1_000
   before(async () => {
     Mock__OVM_L2ERC20Gateway = await smockit(
       await ethers.getContractFactory('OVM_L2ERC20Gateway')
     )
-    Mock__WETH = await smockit(
-      await ethers.getContractFactory('OMV_ETH')
+    // deploy an actual ERC20 contract on L1
+    // @todo: use the actual implementation
+    Factory__L1ERC20 = await (
+      await ethers.getContractFactory('ERC20')
+    ).connect(alice)
+    
+
+    await L1ERC20.deploy(
+      initialSupply,
+      'L1ERC20',
+      18
     )
   })
 
@@ -46,34 +54,72 @@ describe.only('OVM_L1ERC20Gateway', () => {
   // The contract under test. Everything else is a mock.
   let OVM_L1ERC20Gateway: Contract
   beforeEach(async () => {
-    let signer
-    ;[signer, alice, bob] = await ethers.getSigners()
-    // though we don't keep track of the signer here, it needs to have come from an ethers signer so that {from: mockL2...} will work.
-    mockL1CrossDomainMessengerAddress = await signer.getAddress() 
+    let l1MessengerImpersonator: Signer
+    ;[l1MessengerImpersonator, alice, bob] = await ethers.getSigners()
 
     // get a mock L1 messenger
     Mock__OVM_L1CrossDomainMessenger = await smockit(
-      await ethers.getContractFactory('OVM_L2CrossDomainMessenger'),
-      { address: mockL1CrossDomainMessengerAddress }
+      await ethers.getContractFactory('OVM_L1CrossDomainMessenger'),
+      { address: await l1MessengerImpersonator.getAddress() } // This allows us to use an ethers override {from: Mock__OVM_L2CrossDomainMessenger.address} to mock calls
     )
-  
-    // create a mock WETH contract on L1 (just using OVM_ETH)
-    // Mock__WETH = await smockit(
-    //   await ethers.getContractFactory('OVM_ETH')
-    // )
 
     // Deploy the contract under test: 
     OVM_L1ERC20Gateway = await(
       await ethers.getContractFactory('OVM_L1ERC20Gateway')
     ).deploy(
-      Mock__WETH.address,
+      L1ERC20.address,
       Mock__OVM_L2ERC20Gateway.address,
       Mock__OVM_L1CrossDomainMessenger.address
     )
 
   })
 
-  it('works', async () => {
-    expect(true);
+  describe('finalizeWithdrawal', () => {
+    it('onlyFromCrossDomainAccount: should revert on calls from a non-crossDomainMessenger L1 account', async () => {
+      expect(false)
+    })
+
+    it('onlyFromCrossDomainAccount: should revert on calls from the right crossDomainMessenger, but wrong xDomainMessageSender (ie. not the L2ERC20Gateway)', async () => {
+      
+    })
+
+    const depositAmount = 100
+    it('should credit funds to the withdrawer', async () => {
+    })
+  })
+
+  describe('deposits', () => {
+    const INITIAL_TOTAL_SUPPLY = 100_000
+    const ALICE_INITIAL_BALANCE = 50_000
+    const withdrawAmount = 1_000
+    let SmoddedL2Gateway: ModifiableContract
+    beforeEach(async () => {
+      // Deploy a smodded gateway so we can give some balances to withdraw
+      // SmoddedL2Gateway = await (await smoddit('OVM_L2ERC20Gateway', alice)).deploy(
+      //   Mock__OVM_L2CrossDomainMessenger.address,
+      //   'ovmWETH', // here: https://etherscan.io/address/0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2#code
+      //   decimals
+      // )
+      // await SmoddedL2Gateway.init(
+      //   MOCK_L1GATEWAY_ADDRESS
+      // )
+
+      // // Populate the initial state with a total supply and some money in alice's balance
+      // const aliceAddress = await alice.getAddress()
+      // SmoddedL2Gateway.smodify.put({
+      //   totalSupply: INITIAL_TOTAL_SUPPLY,
+      //   balances: {
+      //     [aliceAddress] : ALICE_INITIAL_BALANCE
+      //   }
+      // })
+    })
+
+    it('deposit() escrows the deposit amount and sends the correct deposit message', async () => { 
+      
+    })
+
+    it('depositTo() escrows the deposit amount and sends the correct deposit message', async () => { 
+      
+    })
   })
 })
