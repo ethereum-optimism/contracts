c7cd7a8c5acc3029a4d0d01efbb93c9e204f15ce
Ben Jones <ben@pseudonym.party>
2021-02-14 13:16:46 -0800

    lower gas further

--- contracts/optimistic-ethereum/OVM/bridge/ETH/OVM_L1ETHGateway.sol
+++ contracts/optimistic-ethereum/OVM/bridge/ETH/OVM_L1ETHGateway.sol
@@ -90,7 +90,7 @@ contract OVM_L1ETHGateway is iOVM_L1ETHGateway, OVM_CrossChainEnabled {
         sendCrossDomainMessage(
             l2ERC20Gateway,
             data,
-            1999999 // TODO: meter and set with some buffer
+            1000000 // TODO: meter and set with some buffer
         );
 
         emit DepositInitiated(_from, _to, msg.value);
