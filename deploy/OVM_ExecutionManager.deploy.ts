8682c9b9d0c83a8c42b3b24a8b225cf5c5834ee9
Kelvin Fichter <kelvinfichter@gmail.com>
2021-02-17 19:23:51 -0500

    Start process of converting to hardhat-deploy

---
+++ deploy/OVM_ExecutionManager.deploy.ts
@@ -0,0 +1,33 @@
+import { DeployFunction } from "hardhat-deploy/dist/types";
+
+const deployFn: DeployFunction = async (hre) => {
+    const { deploy } = hre.deployments
+    const { deployer } = await hre.getNamedAccounts()
+
+    const Lib_AddressManager = await hre.deployments.get(
+        'Lib_AddressManager'
+    )
+
+    await deploy('OVM_ExecutionManager', {
+        from: deployer,
+        gasLimit: 4_000_000,
+        args: [
+            Lib_AddressManager.address,
+            {
+                minTransactionGasLimit: 20_000,
+                maxTransactionGasLimit: 9_000_000,
+                maxGasPerQueuePerEpoch: 9_000_000,
+                secondsPerEpoch: 0,
+            },
+            {
+                ovmCHAINID: 10
+            }
+        ],
+    })
+}
+
+deployFn.dependencies = [
+    'Lib_AddressManager'
+]
+
+export default deployFn
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,14 +9,22 @@ import {
 import '@nomiclabs/hardhat-ethers'
 import '@nomiclabs/hardhat-waffle'
 import 'hardhat-typechain'
+import 'hardhat-deploy'
 import '@eth-optimism/plugins/hardhat/compiler'
 import '@eth-optimism/smock/build/src/plugins/hardhat-storagelayout'
 
 const config: HardhatUserConfig = {
+  paths: {
+    deploy: './deploy',
+    deployments: './deployments'
+  },
   networks: {
     hardhat: {
       accounts: DEFAULT_ACCOUNTS_HARDHAT,
       blockGasLimit: RUN_OVM_TEST_GAS * 2,
+      live: true,
+      saveDeployments: true,
+      tags: ["test", "local"]
     },
   },
   mocha: {
@@ -32,6 +40,11 @@ const config: HardhatUserConfig = {
     outDir: 'build/types',
     target: 'ethers-v5',
   },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    }
+  }
 }
 
 export default config
