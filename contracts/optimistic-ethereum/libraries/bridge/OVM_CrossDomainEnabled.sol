a24cb06173050bb112e86f5c02071cb8dab42ad8
Ben Jones <ben@pseudonym.party>
2021-02-25 12:58:45 -0800

    fix imports

--- contracts/optimistic-ethereum/libraries/bridge/OVM_CrossDomainEnabled.sol
+++ contracts/optimistic-ethereum/libraries/bridge/OVM_CrossDomainEnabled.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >0.5.0 <0.8.0;
 /* Interface Imports */
-import { iAbs_BaseCrossDomainMessenger } from "../../iOVM/bridge/messenging/iAbs_BaseCrossDomainMessenger.sol";
+import { iAbs_BaseCrossDomainMessenger } from "../../iOVM/bridge/messaging/iAbs_BaseCrossDomainMessenger.sol";
 
 /**
  * @title OVM_CrossDomainEnabled
