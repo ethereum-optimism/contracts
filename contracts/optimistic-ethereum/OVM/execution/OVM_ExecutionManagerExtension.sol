1dd8fe0f7e4b1f6e7ac3b146661fbc0cb8028991
Kevin Ho <kevinjho1996@gmail.com>
2021-04-01 16:45:49 -0700

    add extension

---
+++ contracts/optimistic-ethereum/OVM/execution/OVM_ExecutionManagerExtension.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+pragma experimental ABIEncoderV2;
+
+/* Contract Imports */
+import { OVM_ExecutionManager } from "./OVM_ExecutionManager.sol";
+
+contract OVM_ExecutionManagerExtention is OVM_ExecutionManager {
+  
+}
