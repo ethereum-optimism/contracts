48ff20a7528b6aa5edc47717da501b08aeb0fa6b
Kelvin Fichter <kelvinfichter@gmail.com>
2021-04-05 18:55:22 -0700

    use gasleft instead of hardcoded gas limit

--- contracts/optimistic-ethereum/OVM/accounts/OVM_ECDSAContractAccount.sol
+++ contracts/optimistic-ethereum/OVM/accounts/OVM_ECDSAContractAccount.sol
@@ -114,7 +114,7 @@ contract OVM_ECDSAContractAccount is iOVM_ECDSAContractAccount {
         // Contract creations are signalled by sending a transaction to the zero address.
         if (decodedTx.to == address(0)) {
             (address created, bytes memory revertData) = Lib_SafeExecutionManagerWrapper.safeCREATE(
-                8999999,
+                gasleft(),
                 decodedTx.data
             );
 
@@ -131,7 +131,7 @@ contract OVM_ECDSAContractAccount is iOVM_ECDSAContractAccount {
             Lib_SafeExecutionManagerWrapper.safeINCREMENTNONCE();
 
             return Lib_SafeExecutionManagerWrapper.safeCALL(
-                8999999,
+                gasleft(),
                 decodedTx.to,
                 decodedTx.data
             );
