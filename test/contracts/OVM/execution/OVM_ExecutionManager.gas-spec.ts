6f6d715c03bfd7ed15fb7d04add1eaff1aede8e7
Ben Jones <ben@pseudonym.party>
2021-04-08 17:46:33 -0700

    fix gas spec

--- test/contracts/OVM/execution/OVM_ExecutionManager.gas-spec.ts
+++ test/contracts/OVM/execution/OVM_ExecutionManager.gas-spec.ts
@@ -110,7 +110,7 @@ describe('OVM_ExecutionManager gas consumption', () => {
       )
       console.log(`calculated gas cost of ${gasCost}`)
 
-      const benchmark: number = 147_500
+      const benchmark: number = 210_000
       expect(gasCost).to.be.lte(benchmark)
       expect(gasCost).to.be.gte(
         benchmark - 1_000,
